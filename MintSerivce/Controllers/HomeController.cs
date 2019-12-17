db5cc3ed9572d266a6da086432074461668ec46c
Verser <verser@outlook.com.au>
2019-12-17 14:59:25 +1100

    nEW UPDATES

--- MintSerivce/Controllers/HomeController.cs
+++ MintSerivce/Controllers/HomeController.cs
@@ -267,45 +267,6 @@ namespace MintSerivce.Controllers
             }
             return ordermodel;
         }
-
-        public static List<OrderDispatchViewModel> DispatchedThnReturnedOrderList()
-        {
-
-            List<OrderDispatchViewModel> ordermodel = new List<OrderDispatchViewModel>();
-            string response = string.Empty;
-            string OnOrderlist = ConfigurationManager.AppSettings["rooturi"] + ConfigurationManager.AppSettings["DispatchedThnReturnedOrders"];
-            string token = System.Web.HttpContext.Current.Session["BearerToken"].ToString();
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                    var resp = client.GetAsync(OnOrderlist);
-
-                    resp.Wait(TimeSpan.FromSeconds(10));
-
-                    if (resp.IsCompleted)
-                    {
-                        if (resp.Result.StatusCode == HttpStatusCode.Unauthorized)
-                        {
-                            Console.WriteLine("Authorization failed. Token expired or invalid.");
-                        }
-                        else
-                        {
-                            response = resp.Result.Content.ReadAsStringAsync().Result;
-
-                            ordermodel = JsonConvert.DeserializeObject<List<OrderDispatchViewModel>>(response);
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-
-            }
-            return ordermodel;
-        }
-
         public static List<SKUStock> AvailableSKU()
         {
             List<SKUStock> ordermodel = new List<SKUStock>();
@@ -443,6 +404,85 @@ namespace MintSerivce.Controllers
             var leftMargin = e.MarginBounds.Left;
             var topMargin = e.MarginBounds.Top;
             e.Graphics.DrawString(PrintContent, printFont, Brushes.Black, leftMargin, topMargin);
+        }
+
+
+        public ActionResult PartialReturnedOrders()
+        {
+            if (Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var dispatchedorders = DispatchedThnReturnedOrderList();
+            return View(dispatchedorders);
+
+        }
+
+        public static List<OrderDispatchViewModel> DispatchedThnReturnedOrderList()
+        {
+
+            List<OrderDispatchViewModel> ordermodel = new List<OrderDispatchViewModel>();
+            string response = string.Empty;
+            string OnOrderlist = ConfigurationManager.AppSettings["rooturi"] + ConfigurationManager.AppSettings["DispatchedThnReturnedOrders"];
+            string token = System.Web.HttpContext.Current.Session["BearerToken"].ToString();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var resp = client.GetAsync(OnOrderlist);
+
+                    resp.Wait(TimeSpan.FromSeconds(10));
+
+                    if (resp.IsCompleted)
+                    {
+                        if (resp.Result.StatusCode == HttpStatusCode.Unauthorized)
+                        {
+                            Console.WriteLine("Authorization failed. Token expired or invalid.");
+                        }
+                        else
+                        {
+                            response = resp.Result.Content.ReadAsStringAsync().Result;
+
+                            ordermodel = JsonConvert.DeserializeObject<List<OrderDispatchViewModel>>(response);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return ordermodel;
+        }
+
+        [HttpPost]
+        public ActionResult ExportDispatchedThnReturnedOrderToExcel()
+        {
+            List<OrderDispatchViewModel> DispatchOrdersList = new List<OrderDispatchViewModel>();
+            if (System.Web.HttpContext.Current.Session["User"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                DispatchOrdersList = DispatchedThnReturnedOrderList();
+                GridView gv = new GridView();
+                gv.DataSource = DispatchOrdersList;
+                gv.DataBind();
+                Response.ClearContent();
+                Response.Buffer = true;
+                Response.AddHeader("content-disposition", "attachment; filename=DispatchedOrdersList.xls");
+                Response.ContentType = "application/ms-excel";
+                Response.Charset = "";
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new System.Web.UI.HtmlTextWriter(sw);
+                gv.RenderControl(htw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+            return RedirectToAction("ReturnedOrders", "ReturnedOrders");
         }
 
         [HttpPost]
