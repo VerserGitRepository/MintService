aa553fe4bb237a7847e0d44d822832745a285672
Verser <verser@outlook.com.au>
2020-07-03 23:11:23 +1000

    Create PDFFileReturnModel.cs

---
+++ MintSerivce/Models/PDFFileReturnModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MintSerivce.Models
+{
+    public class PDFFileReturnModel
+    {
+        public bool IsSuccess { get; set; }
+
+        public byte[] FileContent { get; set; }
+
+        public string ResultMessage { get; set; }
+    }
+}
