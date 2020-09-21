e181cfe63b288f1a01405c805862be2190be78f3
Verser <verser@outlook.com.au>
2020-09-22 09:49:50 +1000

    new changes

---
+++ MintSerivce/Models/ReturnReplacementViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MintSerivce.Models
+{
+    public class ReturnReplacementViewModel
+    {
+        public string CustomerID { get; set; }
+        public DateTime? DateOfPurchase { get; set; }
+        public DateTime? DateOfReturnOrganised { get; set; }
+        public DateTime? DateOfExpectedReturn { get; set; } //Date of return organised +7 days
+        public string ReturnType { get; set; } //DropdownList
+        public string CoolingOffPeriod { get; set; } //Yes, No
+        public string TrackingNumberToCustomer { get; set; }
+        public string TrackingNumberFromCustomerToVerser { get; set; }
+        public int? NumberofdaysReturnedReceive   { get; set; } //-	Number of days taken to receive returned device  ( The date it was received by Verser minus the date of returned organise)
+        public string IsReplaced { get; set; }
+        public string SMSReminder { get; set; } //Yes/No
+    }
+}
